0c4b2d33eca0fdf00fc3182c9d7481ddb29809b6
unknown <you@example.com>
2025-05-09 19:58:18 +0100

    Initial Vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+
+function App() {
+    return (
+
+      <div>
+        <h1>My Home Finder</h1>
+      </div>
+      );
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Home Finder App</h1>
+      <p>Find your dream home with us!</p>
+    </div>
+  );
+}
+
+export default Home;
