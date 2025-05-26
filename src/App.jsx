d1c0026c46993e911564249c8079634126a4dcbe
unknown <you@example.com>
2025-05-26 16:56:13 +0100

    feat: initialize routing with React Router
    Added placeholder components for all defined routes to test navigation and structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Decision from "./pages/Decision";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Listings from "./pages/Listings";
+import ListingDetails from "./pages/ListingDetails";
+import Profile from "./pages/Profile";
+import Messaging from "./pages/Messaging";
+import Reviews from "./pages/Reviews";
 
 function App() {
     return (
+      // Wrapping the application with Router to enable routing
+      // and defining the routes for different pages
+      // Each Route component specifies a path and the corresponding component to render
 
-      <div>
-        <h1>My Home Finder</h1>
-      </div>
-      );
-}
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/decision" element={<Decision />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/listings" element={<Listings />} />
+            <Route path="/listings/:id" element={<ListingDetails />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/messaging" element={<Messaging />} />
+            <Route path="/reviews" element={<Reviews />} />
+          </Routes>
+    );
+};
 
 export default App
---
+++ src/pages/ListingDetails.jsx
@@ -0,0 +1,9 @@
+function ListingDetails() {
+  return (
+    <div>
+      <h1>Listings Page</h1>
+    </div>
+  );
+}
+
+export default ListingDetails;
