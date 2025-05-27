b0a0a274eb793951dd5b73a4b6cc4f8357224c11
arinde <arindevictor8@gmail.com>
2025-05-27 11:46:36 +0100

    feature/Profile-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Home from "./pages/Home";
 
+import ProfileForm from "./Profile/Tenants/ProfileForm";
+
 function App() {
     return (
 
       <div>
         <h1>My Home Finder</h1>
+        
+        <ProfileForm />
       </div>
       );
 }
---
+++ src/Profile/Tenants/TenantProfile.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+
+const TenantProfile = () => {
+    <div className="form-container">
+        <div>
+            <div>
+                <img src=" " alt="" />
+                <p></p>
+            </div>
+            <p></p>
+        </div>
+        <div>
+            <div>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <p></p>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <p></p>
+                <img src="" alt="" />
+            </div>
+        </div>
+        <div>
+            <div>
+                <p></p>
+                <p></p>
+            </div>
+            <div></div>
+            <p></p>
+        </div>
+        <div>
+            <div>
+                <p></p>
+                <a href=""></a>
+            </div>
+            <div></div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default TenantProfile
