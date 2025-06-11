ec4774067d4cbdb59dc6e77d7dc4ea63f3db1b6c
Bridget Odunola Adeyemi <bridgetola143@gmail.com>
2025-06-11 21:16:37 +0100

    implement OTP verification flow with method selection and input validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,35 @@ import PropertyCard from "./components/PropertyHero";
 import PropertyDetails from "./components/PropertyDetails";
 import TopNavbar from "./layout/TopNavbar";
 import Sidebar from "./layout/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import OtpSelection from "./pages/otpSelection";
+import OtpVerification from "./pages/otpVerification";
+
 import LandlordlistingPage from "./pages/LandlordListingPage";
 
 function App() {
   return (
-    <section>
-      {/* <TopNavbar />
-      <div>
-        <Sidebar />
-      </div> */}
-      <LandlordlistingPage />
-    </section>
+    // <section>
+
+    //   <LandlordlistingPage />
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<OtpSelection />} />
+    //       <Route path="/verify" element={<OtpVerification />} />
+    //       <Route
+    //         path="/dashboard"
+    //         element={<h1>Success! You're verified.</h1>}
+    //       />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </section>
+    <Routes>
+      <Route path="/" element={<OtpSelection />} />
+      <Route path="/verify" element={<OtpVerification />} />
+      <Route path="/dashboard" element={<h1>Success! You're verified.</h1>} />
+      <Route path="/landlord" element={<LandlordlistingPage />} />
+      {/* <Route path="*" element={<h1>Page Not Found</h1>} /> */}
+    </Routes>
   );
 }
 
