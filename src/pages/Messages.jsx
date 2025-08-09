7ef12563efe6abf5026b36d5d1aaedc9879b99e2
Opia12345 <preciousopia7@gmail.com>
2025-08-09 18:19:56 +0100

    fix: deployment error

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useSearchParams, useLocation } from "react-router-dom";
 import AllMessages from "../Components/AllMessages";
-import MessageDetails from "../components/MessageDetails";
+import MessageDetails from "../Components/MessageDetails";
 import { useMessaging } from "../contexts/MessagingContext";
 import { useAuth } from "../contexts/AuthContext";
 
